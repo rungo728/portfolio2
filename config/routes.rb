286d8699c25ffdfea850cdf6a1472502e84297be
rungo728 <rufii72865@gmail.com>
2020-03-28 22:21:39 +0900

    setting root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # 仮のルーティング
-  root 'tests#index'
+  root 'articles#index'
   resources :tests
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
